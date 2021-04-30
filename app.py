b6b320a9c2580d39cfb0b08e49fed59fbf124314
LAPTOP-GOHQ1NNM\abea <bea@missionbio.com>
2021-04-30 16:46:12 -0700

    latest update

--- app.py
+++ app.py
@@ -2,37 +2,67 @@ import streamlit as st
 import warnings
 import tkinter as tk
 from tkinter import filedialog
-import Main as main
-from PIL import Image
+import glob
+import Fluoresence as flu
+
 warnings.filterwarnings('ignore')
 
-#INPUTS
-cont_list = []
-Name = st.sidebar.text_input('Name', '')
-Run_ID = st.sidebar.text_input('Run_ID', '')
-Analysis_Type = st.sidebar.selectbox('Type', 
-                                    ('Fluoresence', 'placeholder', 'placeholder'))
-# Tkinter Folder Picker
-root = tk.Tk()
-root.withdraw()
-root.wm_attributes('-topmost', 1)
-input_dire = []
-clicked = st.sidebar.button('Input Folder Picker')
-if clicked:
-    input_dire = st.sidebar.text_input('Selected Input Folder:', filedialog.askdirectory(master=root))
-    input_has_been_clicked = True
-    with st.spinner('You are running Fluoresence Analysis...'):
-        cont_list = main.run(input_dire)
-        if cont_list is not None:
-            for cont in cont_list:
-                if cont is not None:
-                    cont[0].to_csv(input_dire + "/" + "Analyzed" + Name + "_" + Run_ID + "_" + Analysis_Type  + ".csv", index=True)
-                    st.write(cont[0])
-                    for name, img in cont[1]:
-                        st.write("File: ", name)
-                        #st.pyplot(img)
-                        img.savefig(input_dire + "/" + "Analyzed" + name + ".png")
-                        saved_img = Image.open(input_dire + "/" + "Analyzed" + name + ".png")
-                        st.image(saved_img)
-    st.success("Done!")
-    st.balloons()
+def get_all_directory(input_dir):
+    directories = []
+    if input_dir:
+        if input_dir[-1] != '/':
+            input_dir += '/'
+        directories = glob.glob(input_dir + "**/", recursive=True)
+    return directories
+
+def run(input_dire):
+    cont_list = []
+    st.write("Analysis Progress")
+    my_bar = st.progress(0)
+    directories = get_all_directory(input_dire)
+    amount = 1 / len(directories)
+    last_amount = 0
+    if input_dire:
+        for dire in directories:
+            cont = flu.Fluorescence_Controller(input_dire, dire)
+            my_bar.progress(last_amount + amount)
+            last_amount += amount
+            cont_list.append(cont)
+    return cont_list
+
+def main():
+    #INPUTS
+    cont_list = []
+    Name = st.sidebar.text_input('Name', '')
+    Run_ID = st.sidebar.text_input('Run_ID', '')
+    Analysis_Type = st.sidebar.selectbox('Type', ('Fluoresence', ))
+    CSV_Output_Browser = st.sidebar.checkbox("Output CSV to Browser")
+    Image_Output_Browser = st.sidebar.checkbox("Output Images to Browser")
+    # Tkinter Folder Picker #
+    root = tk.Tk()
+    root.withdraw()
+    root.wm_attributes('-topmost', 1)
+    input_dire = []
+    clicked = st.sidebar.button('Input Folder Picker')
+    if clicked:
+        input_dire = st.sidebar.text_input('Selected Input Folder:', filedialog.askdirectory(master=root))
+        with st.spinner('You are running Fluoresence Analysis...'):
+            cont_list = run(input_dire)
+            if cont_list is not None:
+                for cont in cont_list:
+                    if cont is not None:
+                        if CSV_Output_Browser:
+                            cont[0].to_csv(input_dire + "/" + "Analyzed" + Name + "_" + Run_ID + "_" + Analysis_Type  + ".csv", index=True)
+                            st.write(cont[0])
+                        with st.spinner("Displaying images for the run..."):
+                            for name, img in cont[1]:
+                                if Image_Output_Browser:
+                                    st.write("File: ", name)
+                                    st.pyplot(img)
+                                img.savefig(input_dire + "/" + "Analyzed" + name + ".png")
+                        st.success("Images Displayed! Set Complete")
+        st.success("Done!")
+        st.balloons()
+
+if __name__ == '__main__':
+    main()
